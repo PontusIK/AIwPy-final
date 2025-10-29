ec2f972b4a5b28a7e56825361e46a111ce09e2ee
PontusIK <pontus.kavrestad@gmail.com>
2025-10-29 19:16:43 +0100

    fixing warning with input shape

--- model/model.py
+++ model/model.py
@@ -16,7 +16,8 @@ y_train = y_train.flatten()
 y_test = y_test.flatten()
 
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
+    layers.Input(shape=(28, 28, 1)),
+    layers.Conv2D(32, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
