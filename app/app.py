e694ed1bcd4953c6a4078c82009390bc189946a7
PontusIK <pontus.kavrestad@gmail.com>
2025-11-05 17:57:13 +0000

    useless label

--- app/app.py
+++ app/app.py
@@ -13,4 +13,16 @@ header.pack(fill="x", side="top")
 button = tkinter.Button(header, text="Upload Image", command=upload_image)
 button.pack(side="left", padx=10, pady=10)
 
+output = tkinter.Frame(root)
+output.pack(fill="x", side="bottom")
+
+ai_respone = tkinter.StringVar(value="...")
+response_label = tkinter.Label(
+    output,
+    textvariable=ai_respone,
+    anchor="w",
+    padx=10
+)
+response_label.pack(fill="both", padx=10, pady=10)
+
 root.mainloop()
