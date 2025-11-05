75bb17df70018d27a4d30c13b3dbc9318e279566
PontusIK <pontus.kavrestad@gmail.com>
2025-11-05 17:28:26 +0000

    useless button

--- app/app.py
+++ app/app.py
@@ -1,4 +1,16 @@
 import tkinter
 
-tk = tkinter.Tk()
-tk.mainloop()
+def upload_image():
+    pass
+
+root = tkinter.Tk()
+root.title("HTR Demo")
+root.geometry("900x720")
+
+header = tkinter.Frame(root)
+header.pack(fill="x", side="top")
+
+button = tkinter.Button(header, text="Upload Image", command=upload_image)
+button.pack(side="left", padx=10, pady=10)
+
+root.mainloop()
